d2c47247cff50a72b1f92f9d21808ca2dc1e3c3c
Victor Carvalho <victor.blq@gmail.com>
2019-05-25 23:22:38 -0300

    arquivos corretos

---
+++ build/precache-manifest.4e0bc20b4478e1026315e93d268ad610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e210ebab474cd67f6bc40b7000961a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4e4eb4d556ac25ba9b",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "ae4e4eb4d556ac25ba9b",
+    "url": "/static/js/main.6a4f7d39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
