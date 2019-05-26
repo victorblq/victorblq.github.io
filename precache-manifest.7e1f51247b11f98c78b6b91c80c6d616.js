c2a3a3ff069e1c7bcce147764c591c797b5dc154
Victor Carvalho <victor.blq@gmail.com>
2019-05-25 23:50:30 -0300

    Updates

--- precache-manifest.7e1f51247b11f98c78b6b91c80c6d616.js
+++ precache-manifest.7e1f51247b11f98c78b6b91c80c6d616.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e210ebab474cd67f6bc40b7000961a35",
+    "revision": "ff43e42e35a286169ea80dd685a67856",
     "url": "/index.html"
   },
   {
-    "revision": "ae4e4eb4d556ac25ba9b",
-    "url": "/static/css/main.0ebde3ef.chunk.css"
+    "revision": "21df98a6f618d7450fa5",
+    "url": "/static/css/main.ff7df1a5.chunk.css"
   },
   {
     "revision": "685d0c597e8419129599",
     "url": "/static/js/2.0e3f6a1a.chunk.js"
   },
   {
-    "revision": "ae4e4eb4d556ac25ba9b",
-    "url": "/static/js/main.6a4f7d39.chunk.js"
+    "revision": "21df98a6f618d7450fa5",
+    "url": "/static/js/main.4ec45907.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
